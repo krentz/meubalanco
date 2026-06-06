43953fd69ac67efcf79327d20e48d764e738b951
Rafael Krentz Gonçalves <rafaelkrentz@MacBook-Air-de-Rafael.local>
2026-06-06 11:45:44 -0300

    web page test

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE = 'meubalanco-v1';
+const ASSETS = [
+  '/meubalanco/index.html',
+  '/meubalanco/manifest.json',
+  '/meubalanco/icon-192.png',
+  '/meubalanco/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
